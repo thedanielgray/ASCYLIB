6d121725f1e21820e00daeef4b272613aa01aecd
Daniel Gray <daniel.grayvargas@epfl.ch>
2016-06-14 16:03:53 +0200

    Add CSV conversion script for LPD Xeon server.

--- export_cpp_data_to_csv.sh
+++ export_cpp_data_to_csv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for data_file in `ls scripts/cpp/data/lpd48core.*.dat`; do
+for data_file in `ls scripts/cpp/data/lpdxeon2680.*.dat`; do
 	echo $data_file
 	csv_file=`echo $data_file | sed -r 's/dat$/csv/'`
 	cat $data_file | sed -r 's/\s+/,/g' > $csv_file
